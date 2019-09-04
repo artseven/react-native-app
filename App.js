40787a2062e3822d66dffafb872043fda6860902
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-04 10:23:15 -0400

    adding handlers to manage state

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
-          placeholder="Course Goal"
-          style={styles.input}
+          placeholder="Course Goal" 
+          style={styles.input} 
+          onChangeText={goalInputHandler} 
+          value={enteredGoal}
         />
-        <Button title="ADD"/>
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
 
@@ -23,14 +36,14 @@ const styles = StyleSheet.create({
     padding: 50
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between', 
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   input: {
-    width: '75%', 
-    borderBottomColor: 'black', 
-    borderBottomWidth: 1, 
-    padding: 10 
+    width: "75%",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    padding: 10
   }
 });
