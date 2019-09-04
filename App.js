b11d2ce8881591492de11b5809ff9dacb3dcd2a1
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-04 15:36:07 -0400

    done

--- App.js
+++ App.js
@@ -9,8 +9,7 @@ import GoalInput from "./components/GoalInput";
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
   const [isAddMode, setIsAddMode] = useState(false);
-  console.log('RE-RENDERING COMPONENT');
-  console.log(courseGoals);
+
 
   const addGoalHandler = goalTitle => {
     if (goalTitle.length === 0) {
@@ -33,8 +32,6 @@ export default function App() {
   };
 
   const removeGoalHandler = goalId => {
-    console.log('TO BE DELETED: ', goalId);
-    console.log(courseGoals);
     setCourseGoals(currentGoals => {
       return currentGoals.filter(goal => goal.id !== goalId);
     });
